fa0068a7e3bee907a3782bd8eba5269f0151159b
Ishaan Desai <ishaandesai@gmail.com>
2022-12-07 14:53:13 +0100

    Code for version 0.2.0

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-//#include "flatset/flatset.hpp"
-//#include "filesystem/filesystem.hpp"
+#include "flatset/flatset.hpp"
+#include "filesystem/filesystem.hpp"
 //#include "yamlParser/yamlParser.hpp"
 #include <iostream>
 
@@ -7,13 +7,13 @@ int main(int argc, char *argv[])
 {
   std::cout << "Let's fight with CMake, Docker, and some dependencies!" << std::endl << std::endl;
 
-  //std::cout << "Modify a flat set using boost container" << std::endl;
-  //modifyAndPrintSets();
-  //std::cout << std::endl;
+  std::cout << "Modify a flat set using boost container" << std::endl;
+  modifyAndPrintSets();
+  std::cout << std::endl;
 
-  //std::cout << "Inspect the current directory using boost filesystem" << std::endl;
-  //inspectDirectory();
-  //std::cout << std::endl;
+  std::cout << "Inspect the current directory using boost filesystem" << std::endl;
+  inspectDirectory();
+  std::cout << std::endl;
 
   //if ( argc == 2 )
   //{
