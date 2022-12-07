0080573717839b9c9b543992f264c726dae5714f
Ishaan Desai <ishaandesai@gmail.com>
2022-12-07 14:50:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Packaging with Spack
+
+Repository for the [Spack exercise](https://github.com/Simulation-Software-Engineering/Lecture-Material/blob/main/03_building_and_packaging/spack_exercise.md). The code is a slightly modified version of the [code used in the CMake exercise](https://github.com/Simulation-Software-Engineering/cmake-exercise-wt2223).
+
+The directory `docker/` contains the recipe of the Docker image that has been prepared for the exercise. Build a Docker image from the recipe and start a container from this image.
---
+++ main.cpp
@@ -0,0 +1,28 @@
+//#include "flatset/flatset.hpp"
+//#include "filesystem/filesystem.hpp"
+//#include "yamlParser/yamlParser.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  std::cout << "Let's fight with CMake, Docker, and some dependencies!" << std::endl << std::endl;
+
+  //std::cout << "Modify a flat set using boost container" << std::endl;
+  //modifyAndPrintSets();
+  //std::cout << std::endl;
+
+  //std::cout << "Inspect the current directory using boost filesystem" << std::endl;
+  //inspectDirectory();
+  //std::cout << std::endl;
+
+  //if ( argc == 2 )
+  //{
+  //  const std::string yamlFile( argv[1] );
+  //  std::cout << "Parse some yaml file with yaml-cpp" << std::endl;
+  //  std::cout << "  " << yamlFile << std::endl;
+  //  parseConfig( yamlFile );
+  //}
+
+  return 0;
+}
+
